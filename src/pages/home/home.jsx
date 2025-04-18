54ac3880c4eaa4818bb8ef1901d00fc244258260
Asfandyar <asfandm17@gmail.com>
2025-04-18 16:00:36 +0500

    home page started

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,14 @@
+import "./home.scss";
+
+const HomePage = () => {
+  return (
+    <div className="homePage">
+      <div className="text-container">text</div>
+      <div className="image-container">
+        <img src="/bg.png" alt="background" />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
